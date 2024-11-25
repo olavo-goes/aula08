72f858888c618e09aee3fda7571a42b7e5b301b8
olavo-goes <olavo.goes@estudante.ifms.edu.br>
2024-11-25 14:22:13 -0400

    salvar
    salvar

--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -3,7 +3,37 @@ import { useState } from "react";
 export default function Registrar() {
 
 
+  const [nome, setNome] = useState('');
+  const [Email, setEmail] = useState('');
+
+
+  event.preventDefault();
+
+
+  const registrar = async () => {
+  try{
+    await fetch('http://localhost:3000/usuarios', {
+        method: 'POST',
+        headers: {'Content-Type': 'application.json'},
+        dody: JSON.stringify({
+          nome: 'nome',
+          email: 'email'
+        })
+    })
+  }
+  catch{
+    alert("ocorreu um erro")
+  } registrar(), []}
+
+
   return (
-        
+        <>
+        <main>
+
+
+
+
+        </main>        
+        </>
   );
 }
